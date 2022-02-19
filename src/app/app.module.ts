5856ed64611e5eb65e559ad48b6643503975de08
ajax <ajaxajaxcarlos@gmail.com>
2022-02-18 19:30:33 -0600

    se crea estructura de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressComponent,
+    DashboardComponent,
+    Grafica1Component,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
